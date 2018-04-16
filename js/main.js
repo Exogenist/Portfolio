e0c90580cf836a66bf3e37795adccda898432304
exogenist <swainson.holness@gmail.com>
2018-04-15 22:04:54 -0600

    bug fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var closeBtn = document.querySelector(".close");
     var sideNav = document.querySelector(".sidenav");
     var hamburger = document.querySelector(".ham-box");
-    var logo = document.querySelector(".sideNavLogoIn");
+//    var logo = document.querySelector(".sideNavLogoIn");
     var links = document.querySelector(".sidenav a");
     var background = document.querySelector(".overlay");
     var token = false;
@@ -12,7 +12,7 @@ window.onload = function () {
         background.style.cursor = "default";
         token = false;
         sideNav.style.width = "0px";
-        logo.style.opacity = "0";
+//        logo.style.opacity = "0";
         background.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
     }
 
@@ -24,8 +24,8 @@ window.onload = function () {
         event.preventDefault();
         background.style.cursor = "pointer";
         sideNav.style.width = "280px";
-        logo.style.opacity = "1";
-        background.style.zIndex = "1";
+//        logo.style.opacity = "1";
+        background.style.zIndex = "3";
 //        sideNav.style.zIndex = "1";
         background.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
         token = true;
