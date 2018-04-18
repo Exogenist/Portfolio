98c4a75116b599495375f89c434f8df3cb2ed8b2
exogenist <swainson.holness@gmail.com>
2018-04-17 20:39:54 -0600

    scroll spy and scroll trigger anim

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var closeBtn = document.querySelector(".close");
     var sideNav = document.querySelector(".sidenav");
     var hamburger = document.querySelector(".ham-box");
-//    var logo = document.querySelector(".sideNavLogoIn");
+    //    var logo = document.querySelector(".sideNavLogoIn");
     var links = document.querySelector(".sidenav a");
     var background = document.querySelector(".overlay");
     var token = false;
@@ -12,7 +12,7 @@ window.onload = function () {
         background.style.cursor = "default";
         token = false;
         sideNav.style.width = "0px";
-//        logo.style.opacity = "0";
+        //        logo.style.opacity = "0";
         background.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
     }
 
@@ -24,9 +24,9 @@ window.onload = function () {
         event.preventDefault();
         background.style.cursor = "pointer";
         sideNav.style.width = "280px";
-//        logo.style.opacity = "1";
+        //        logo.style.opacity = "1";
         background.style.zIndex = "3";
-//        sideNav.style.zIndex = "1";
+        //        sideNav.style.zIndex = "1";
         background.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
         token = true;
     });
@@ -41,6 +41,45 @@ window.onload = function () {
         }
     });
 
+    //    init parallax
     $('.header').parallaxie();
     $('.intrelude').parallaxie();
+
+    //        scroll animation
+    $(this).scrollTop(0);
+    scroll = $(window).scrollTop();
+    $(window).scroll(function () {
+        scroll = $(window).scrollTop();
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+            if (pos < scroll + 840) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
 }
